3c73719b415165b31741856b76b418480e83f111
David Hernandez Bahr <dhernandez@ingsw.com>
2024-02-13 09:57:28 -0300

    Removing sort from tags input selection.

--- app/inputs/tags_input.rb
+++ app/inputs/tags_input.rb
@@ -52,4 +52,14 @@ class TagsInput < ActiveAdminAddons::InputBase
       end
     end
   end
+
+  def array_to_select_options
+    selected_values = input_value.is_a?(Array) ? input_value : input_value.to_s.split(",")
+    array = collection.map(&:to_s) + selected_values
+    array.map do |value|
+      option = { id: value, text: value }
+      option[:selected] = "selected" if selected_values.include?(value)
+      option
+    end.uniq
+  end
 end
